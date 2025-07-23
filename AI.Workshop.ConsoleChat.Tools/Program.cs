63546822f32d49cb466a56e69a968ddf9e5a672b
Ratko Ćosić <ratko_cosic@hotmail.com>
2025-07-23 14:11:14 +0200

    tools console

---
+++ AI.Workshop.ConsoleChat.Tools/Program.cs
@@ -0,0 +1,13 @@
+﻿using AI.Workshop.ConsoleChat.Tools;
+
+Console.WriteLine("Welcome to the AI Workshop Console Chat Tools examples!\r\n");
+
+var tools = new BasicToolsExamples();
+
+// 1) call a tool method directly
+//await tools.ItemPriceMethod();
+
+// 2) call a tool method using the shopping cart methods to simulate state
+await tools.ShoppingCartMethods();
+
+
