721ecdaf856b65f41701f67cb59ad9b11936ae38
Ratko Ćosić <ratko_cosic@hotmail.com>
2025-07-24 08:59:55 +0200

    rag

---
+++ AI.Workshop.ConsoleChat.RAG/AzureSearchExamples.cs
@@ -0,0 +1,47 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+
+namespace AI.Workshop.ConsoleChat.RAG
+{
+    internal class AzureSearchExamples
+    {
+        private readonly AzureOpenAIClient _client;
+        private readonly IEmbeddingGenerator<string, Embedding<float>> _generator;
+        protected readonly IChatClient _chatClient;
+
+        public AzureSearchExamples()
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .AddJsonFile("Prompts.json", false, false)
+                .Build();
+
+            var openAiEndpoint = config["AZURE_OPENAI_ENDPOINT"];
+            var openAiKey = config["AZURE_OPENAI_KEY"];
+            var deployment = config["AZURE_EMBEDDING_DEPLOYMENT"];
+            var searchEndpoint = config["AZURE_SEARCH_ENDPOINT"];
+            var searchKey = config["AZURE_SEARCH_KEY"];
+
+            _client = new AzureOpenAIClient(new Uri(openAiEndpoint), new AzureKeyCredential(openAiKey));
+
+            var embeddingClient = _client.GetEmbeddingClient(deployment);
+
+            _generator = embeddingClient.AsIEmbeddingGenerator();
+        }
+
+        internal async Task GenerateEmbeddingForQueryAsync(string text)
+        {
+            var embedding = await _generator.GenerateAsync(text);
+
+            Console.WriteLine($"Embedding dimension: {embedding.Vector.Length}");
+            Console.WriteLine($"First 10 values: {string.Join(", ", embedding.Vector.ToArray().Take(10))}");
+
+            ReadOnlyMemory<float> vector = await _generator.GenerateVectorAsync(text);
+
+            vector.ToArray().AsSpan(0, 5).ToArray().ToList()
+                .ForEach(value => Console.WriteLine($"Vector value: {value}"));
+        }
+    }
+}
--- AI.Workshop.ConsoleChat.RAG/Program.cs
+++ AI.Workshop.ConsoleChat.RAG/Program.cs
@@ -2,9 +2,19 @@
 
 Console.WriteLine("Welcome to the AI Workshop Console Chat RAG examples!\r\n");
 
+/*
 var search = new InMemoryVectorStoreSearch();
 
 await search.GenerateVectorsAsync();
 await search.SearchAsync("Which Azure service should I use to store my Word documents?");
+
+*/
+
+var userPrompt = "I'm testing. Fetch me one article from your knowledge base, a seminar from inhalt index and tell me the current time.";
+
+var search = new AzureSearchExamples();
+
+await search.GenerateEmbeddingForQueryAsync(userPrompt);
+
 
 
