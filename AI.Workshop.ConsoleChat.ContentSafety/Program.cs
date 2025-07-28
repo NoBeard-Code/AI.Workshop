cb8f0a64c29b312a41ec3c595f377d95f9028602
Ratko Ćosić <ratko_cosic@hotmail.com>
2025-07-28 15:46:07 +0200

    content safety methods

---
+++ AI.Workshop.ConsoleChat.ContentSafety/Program.cs
@@ -0,0 +1,7 @@
+﻿using AI.Workshop.ConsoleChat.ContentSafety;
+
+//var foundryEvaluation = new AzureFoundryEvaluationService();
+//await foundryEvaluation.EvaluateContentSafetyAsync("Tell me a joke");
+
+var evaluation = new AzureContentSafetyService();
+await evaluation.EvaluateContentSafetyAsync();
