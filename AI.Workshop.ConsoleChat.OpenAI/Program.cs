78d5979664ebce122633553880d405bd14066e4f
Ratko Ćosić <ratko_cosic@hotmail.com>
2025-11-11 12:03:05 +0100

    all in

--- AI.Workshop.ConsoleChat.OpenAI/Program.cs
+++ AI.Workshop.ConsoleChat.OpenAI/Program.cs
@@ -2,7 +2,6 @@
 
 Console.WriteLine("Welcome to the AI Workshop Console Chat OpenAI examples!\r\n");
 
-/*
 var basics = new BasicChatExamples();
 
 // 1) the basic usage is to request a chat response:
@@ -19,9 +18,7 @@ await basics.RequestStreamingChatResponse();
 
 // 5) Request a streaming chat response with history:
 await basics.RequestStreamingChatResponseWithHistory();
-*/
 
-/*
 var builderExamples = new ChatClientBuilderExamples();
 
 // 1) Tool calling example:
@@ -32,9 +29,7 @@ await builderExamples.DistributedCache();
 
 // 3) OpenTelemetry example:
 await builderExamples.UseTelemetry();
-*/
 
-/*
 var middleware = new ChatClientMiddlewareExamples();
 
 // 1) Basic usage with RateLimitingChatClient:
@@ -45,9 +40,7 @@ await middleware.UsageWithMiddleware();
 
 // 3) Usage without custom class:
 await middleware.UsageWithoutCustomClass();
-*/
 
-/*
 var stateful = new StatefulServiceExamples();
 
 // 1) Stateless prompts example:
@@ -58,9 +51,7 @@ await stateful.SendStatefulPromptsExplicitly();
 
 // 3) Implicitly stateful prompts example:
 await stateful.SendStatefulPromptsImplicitly();
-*/
 
-/*
 var embeddings = new EmbeddingExamples();
 
 // 1) Generate embeddings for a single text:
@@ -74,26 +65,23 @@ await embeddings.GenerateVectorFromInput();
 
 // 4) Use a delegating embedding generator with rate limiting:
 await embeddings.UseDelegatingEmbeddingGenerator();
-*/
-
 
 var more = new MoreChatExamples();
 
-// 1) Hiking chat example:
-//await more.HikingChat();
+//1) Hiking chat example:
+await more.HikingChat();
 
-// 2) Sentiment analysis example:
-//await more.RequestStructuredOutput();
+//2) Sentiment analysis example:
+await more.RequestStructuredOutput();
 
-// 3) Sentiment analysis for multiple inputs:
-//await more.SentimentAnalysisForMoreInputs();
+//3) Sentiment analysis for multiple inputs:
+await more.SentimentAnalysisForMoreInputs();
 
 // 4) Chat with biased responses:
 await more.TryToSellSocksWithChat(true);
 
-/*
 var semanticSearch = new SemanticSearchExamples();
 
 // 1) Vector search example:
 await semanticSearch.VectorSearch();
-*/
+
